98d440f79214ea5cad1b03aad2d27297d13e2ae3
SACHIN <ghoshsachin2301@gmail.com>
2024-03-10 00:02:38 +0530

    Profile pic removed and replaced by icon

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -212,12 +212,13 @@ const Navbar = () => {
         {token && (
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img
+            {/* <div className="w-10 rounded-full"> */}
+                <PiUser size={28} />
+              {/* <img
                 alt="Tailwind CSS Navbar component"
                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
+              /> */}
+            {/* </div> */}
           </div>
           <ul className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
             {/* <li>
